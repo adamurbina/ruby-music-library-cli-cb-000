4737d7d17cfd270e4f623596cfbe164d6ea86043
Adam Urbina <adamurbina@gmail.com>
2018-06-29 18:13:32 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,7 @@
 module Concerns
     module Findable
         def find_by_name(name)
-            match = nil
-            self.all.each {|item|
-            if item.name == name
-                match = item
-            end}
-            match
+            self.all.detect {|item| item.name == name}
         end
 
         def find_or_create_by_name(name)
