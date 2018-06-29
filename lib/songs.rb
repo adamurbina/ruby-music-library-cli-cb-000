0ba125971f5fae9069f46e40bd620e35acf28b07
Adam Urbina <adamurbina@gmail.com>
2018-06-29 20:18:25 +0000

    automatically backed up by learn

--- lib/songs.rb
+++ lib/songs.rb
@@ -39,13 +39,7 @@ class Song
     end
 
     def self.find_by_name(name)
-        match = nil
-        self.all.each{|song|
-        if song.name == name
-            match = song
-        end}
-        match
-        self.all.detect{|s| s.name == name}
+        self.all.detect{|song| song.name == name}
     end
 
     def self.find_or_create_by_name(name)
